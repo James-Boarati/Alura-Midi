36c0ec995cae63a9fcb401ce0fc489584866c7cf
James Boarati <runaway_james@hotmail.com>
2022-11-12 01:10:37 -0300

    Funções e textos dinâmicos

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
+const ListaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+while (contador < ListaDeTeclas.length) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+}
